514d242ead166f4f3290c869838f71fc96bbf5a5
Yaroslava <yaroslava.hovenko@gmail.com>
2023-11-01 22:55:46 +0200

    fix task from guide

--- js/HW.js
+++ js/HW.js
@@ -103,6 +103,47 @@
 // console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
 //----------------------------------------------------------------------------------------- МОДУЛЬ 3 FIXME: ALl three task rest
 
+//TASK 1
+
+// function slugify(title) {
+//     return title.toLowerCase().split(' ').join('-');
+// }
+
+// console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+// console.log(slugify('English for developer')); // "english-for-developer"
+// console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+// console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+
+//TASK 2
+// function makeArray(firstArray, secondArray, maxLength) {
+//     return firstArray.concat(secondArray).splice(0, maxLength);
+// }
+
+// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+
+// TASK 3
+
+// function filterArray(numbers, value) {
+//     let arr = [];
+//     for (const num of numbers) {
+//         if (value < num) {
+//             arr.push(num);
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
 //-----------------------------------------------------------------------------------------МОДУЛЬ 4
 
 //TASK 1 Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.________________________________________________
