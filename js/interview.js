6ae1a539f16aec97f2eb9b025e667788e571aa0f
Yaroslava <yaroslava.hovenko@gmail.com>
2023-11-11 22:35:47 +0200

    fix solution

--- js/interview.js
+++ js/interview.js
@@ -864,17 +864,13 @@ const createMatrix = function (row, colum) {
 //ЗАДАЧА 39 знайти мін  і макс з кодеварса (на кодеварсі через мас вирішила)_____________________________________________
 
 // function minMax(arr) {
-//     let min = arr[0];
-//     let max = arr[0];
-//     for (const n of arr) {
-//         if (n < min) {
-//             min = n;
-//         }
-//         if (n > max) {
-//             max = n;
-//         }
+//     let min = arr[0],
+//         max = arr[0];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < min) min = arr[i];
+//         if (arr[i] > max) max = arr[i];
 //     }
-//     return [min, max || max];
+//     return [min, max];
 // }
 
 // console.log(minMax([1, 2, 3, 4, 59, 8, 36])); //[1,5]
