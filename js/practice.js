30d3e6c7b87348c43e04729458c0faa0af2cd5f8
Yaroslava <yaroslava.hovenko@gmail.com>
2023-11-19 23:55:36 +0200

    update

--- js/practice.js
+++ js/practice.js
@@ -9,7 +9,7 @@
 // //     }
 // // }
 
-// ///////////////////////////////
+// /////////////////////////////// 2-й варіант
 
 // for (let i = array.length - 1; i >= 0; i -= 1) {
 //     if (typeof array[i] !== 'number') {
@@ -36,16 +36,49 @@
 
 //ЗАДАЧА 3 ВІДІБРАТИ ЛИШЕ ЖІНОЧІ ІМЕНА З МАСИВУ_________________________
 
-const users = ['Artem', 'Anna', 'Larisa', 'Maksim', 'Svetlana', 'Roman'];
-const men = ['Artem', 'Maksim', 'Roman'];
+// const users = ['Artem', 'Anna', 'Larisa', 'Maksim', 'Svetlana', 'Roman'];
+// const men = ['Artem', 'Maksim', 'Roman'];
 
-function getWomen(users, men) {
-    let women = [];
-    for (let i = 0; i < users.length; i += 1) {
-        if (!men.includes(users[i])) {
-            women.push(users[i]);
-        }
-    }
-    return women;
-}
-console.log(getWomen(users, men));
+// function getWomen(users, men) {
+//     let women = [];
+//     for (const user of users) {
+//         if (!men.includes(user)) {
+//             women.push(user);
+//         }
+//     }
+//     return women;
+// }
+// console.log(getWomen(users, men));
+
+//ЗАДАЧА 4 ПЕРЕВІРИТИ ЧИ ЕЛЕМЕНТИ РОЗТАШОВАНІ В ПОРЯДКУ ЗРОСТАННЯ ЯКЩО НІ ЗАМІНИТИ НА ВІРНІ____________________________
+
+// // const arr = [1, 2, 3, 1, 5, 6, 1, 1, 9]; // 1,2,3,4,5,6,7,8,9
+// const arr = [1, 2, 34, 35, 36, 37, 38, 39];
+
+// for (let i = 1; i < arr.length; i++) {
+//     const currentEl = arr[i];
+//     const prevEl = arr[i - 1];
+
+//     // if (currentEl - prevEl !== 1) {
+//     //     arr[i] = prevEl + 1;
+//     // }
+//     // трішки друга умова
+//     if (currentEl <= prevEl) {
+//         arr[i] = prevEl + 1;
+//     }
+// }
+// console.log(arr);
+
+//ЗАДАЧА 5 СОРТУВАННЯ В АЛФАВІТНОМУ ПОРЯДКУ (сортує не повністью)___________________________
+
+// const langs = ['python', 'javascript', 'c++', 'haskel', 'php', 'ruby', 'avacode', 'bootstrap'];
+// // const newArr = [];
+// for (let i = 0; i < langs.length; i += 1) {
+//     if (langs[0] > langs[i]) {
+//         const result = langs.splice(i, 1)[0]; // перевотврюємо в рядок [0]
+//         langs.unshift(result);
+//     }
+// }
+// console.log(langs);
+
+//  ЗАДАЧА ПОВЕРНУТИ ЗА КЛЮЧЕМ ВЛАСТИВІСТЬ У ВИГЛЯДІ МАСИВУ________________________________
