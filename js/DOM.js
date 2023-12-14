fd992e61fc150a0a3b071b2ea637470d4453dddc
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-14 19:04:01 +0200

    finish with base command

--- js/DOM.js
+++ js/DOM.js
@@ -17,6 +17,14 @@
 //(частіше за все зберігають ID)
 //зберігаються у псевдомасиві Dataset
 //починати назвe обовязково з data потім через девіз рандомна назва (data-action)
+
+//робота з класами відбуваєтся через
+//classList:
+//--add -- динамічно додати клас (при стилізації)
+//--remove -- динамічно видалити
+//--toggle -- видаляє якщо є і додає якщо немає (open/close modal)
+//--replace -- заміна одного класу на інший
+//--contains -- = includes //true/false
 /**
  * 
 elem.parentNode — містить посилання на батьківський вузол-елемент вузла elem
@@ -38,49 +46,59 @@ elem.nextElementSibling — містить посилання на наступ�
 //-- назвою класа
 //-- за ID --- більш надійніше
 
-const buttonEl = document.querySelectorAll('button');
-const reallyArr = [...buttonEl]; //перетворили псевдомасив в масив
-console.log(buttonEl);
-console.log(reallyArr);
-const buttonClass = document.querySelector('.js-btn-less');
-console.log(buttonClass);
-const buttonId = document.querySelector('#zero');
-console.log(buttonId);
+// const buttonEl = document.querySelectorAll('button');
+// const reallyArr = [...buttonEl]; //перетворили псевдомасив в масив
+// console.log(buttonEl);
+// console.log(reallyArr);
+// const buttonClass = document.querySelector('.js-btn-less');
+// console.log(buttonClass);
+// const buttonId = document.querySelector('#zero');
+// console.log(buttonId);
 
-//ЗАМІНА КАРТИНКИ
+// //ЗАМІНА КАРТИНКИ
 
-const ourPicture = document.querySelector('img');
-console.dir(ourPicture);
-ourPicture.src = 'https://i.pinimg.com/236x/0e/bd/26/0ebd262c4b7f69f7ec915dbd8509328f.jpg';
-ourPicture.alt = 'New picture';
+// const ourPicture = document.querySelector('img');
+// console.dir(ourPicture);
+// ourPicture.src = 'https://i.pinimg.com/236x/0e/bd/26/0ebd262c4b7f69f7ec915dbd8509328f.jpg';
+// ourPicture.alt = 'New picture';
 
-const textHead = document.querySelector('.text');
-console.dir(textHead);
-textHead.textContent = 'New text';
+// const textHead = document.querySelector('.text');
+// console.dir(textHead);
+// textHead.textContent = 'New text';
 
-// АТРИБУТИ (src/type/class) -- розширують тег
+// // АТРИБУТИ (src/type/class) -- розширують тег
 
-console.log(ourPicture.getAttribute('src')); // -- отримати атрибут
-ourPicture.setAttribute('alt', 'the next text'); // встановити нове значення атрибута (атрибут, значення)
-ourPicture.removeAttribute('alt'); // видалення атрибуту
-console.log(ourPicture.hasAttribute('alt')); // перевірка чи є такий атрибут
+// console.log(ourPicture.getAttribute('src')); // -- отримати атрибут
+// ourPicture.setAttribute('alt', 'the next text'); // встановити нове значення атрибута (атрибут, значення)
+// ourPicture.removeAttribute('alt'); // видалення атрибуту
+// console.log(ourPicture.hasAttribute('alt')); // перевірка чи є такий атрибут
 
-//ДАТА АТРИБУТИ -- дозволяють якусь інформацію зберігати в тегах
-//(частіше за все зберігають ID)
-//зберігаються у псевдомасиві Dataset
-//починати назвe обовязково з data потім через девіз рандомна назва (data-action)
+// //ДАТА АТРИБУТИ -- дозволяють якусь інформацію зберігати в тегах
+// //(частіше за все зберігають ID)
+// //зберігаються у псевдомасиві Dataset
+// //починати назвe обовязково з data потім через девіз рандомна назва (data-action)
 
-const action = document.querySelectorAll('.actions button'); // КЛАС + ТЕГ щоб був масив
-console.log(action[1].dataset);
-console.log(action[1].dataset.action);
-console.log(action[2].dataset.actionBtn);
+// const action = document.querySelectorAll('.actions button'); // КЛАС + ТЕГ щоб був масив
+// console.log(action[1].dataset);
+// console.log(action[1].dataset.action);
+// console.log(action[2].dataset.actionBtn);
 
-//КЛАСИ
+// //КЛАСИ
 
-//робота з класами відбуваєтся через
-//classList:
-//--add
-//--remove
-//--toggle --
-//--replace
-//--contains
+// //робота з класами відбуваєтся через
+// //classList:
+// //--add -- динамічно додати клас (при стилізації)
+// //--remove -- динамічно видалити
+// //--toggle -- видаляє якщо є і додає якщо немає (open/close modal)
+// //--replace -- заміна одного класу на інший
+// //--contains -- = includes //true/false
+
+// const newElement = document.querySelector('.our-examples');
+
+// newElement.classList.add('new-variant');
+// newElement.classList.remove('our-examples');
+// newElement.classList.toggle('our-examples');
+// newElement.classList.replace('new-variant', 'replace-class');
+// console.log(newElement.classList.contains('new-variant'));
+
+// console.log(newElement);
