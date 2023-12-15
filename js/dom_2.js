51bfd3d6b09108b9ca885ca7532583cb3e70e3fb
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-16 00:00:45 +0200

    add event 'click'

--- js/dom_2.js
+++ js/dom_2.js
@@ -1,3 +1,16 @@
-//=============================ПОДІЇ
+//=============================EVENTS / ПОДІЇ
 
 // 1. CLICK
+const clickBtn = document.querySelector('.main-btn');
+const box = document.querySelector('.box-container');
+
+let step = 0;
+clickBtn.addEventListener('click', event => {
+    // console.log(event);
+    // console.log(event.target);
+    step += 50;
+    box.style.marginTop = `${step}px`;
+    box.style.marginLeft = `${step}px`;
+});
+
+// 2. WORK WITH INPUT
