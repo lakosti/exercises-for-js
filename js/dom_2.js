e8d88bc394f22b26244b7c74f6f24b7f91eb0ff1
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-16 12:28:28 +0200

    add event input

--- js/dom_2.js
+++ js/dom_2.js
@@ -1,16 +1,43 @@
 //=============================EVENTS / ПОДІЇ
 
-// 1. CLICK
-const clickBtn = document.querySelector('.main-btn');
-const box = document.querySelector('.box-container');
+// 1. CLICK / подія клік______________
 
-let step = 0;
-clickBtn.addEventListener('click', event => {
-    // console.log(event);
-    // console.log(event.target);
-    step += 50;
-    box.style.marginTop = `${step}px`;
-    box.style.marginLeft = `${step}px`;
-});
+// const clickBtn = document.querySelector('.main-btn');
+// const box = document.querySelector('.box-container');
 
-// 2. WORK WITH INPUT
+// let step = 0;
+// clickBtn.addEventListener('click', event => {
+//     // console.log(event);
+//     // console.log(event.target);
+//     // box.style.backgroundColor = 'red';
+//     step += 50;
+//     box.style.marginTop = `${step}px`;
+//     box.style.marginLeft = `${step}px`;
+// });
+
+// 2. INPUT / подія інпут__________________
+//---------------------------------------------------------------------------------
+// -- Подія input (ВИВОДИТЬСЯ В КОНСОЛЬ ЩО РАЗУ КОЛИ ПИШЕМО СИМВОЛ)
+
+// const input = document.querySelector('.input-label');
+// console.log(input);
+// input.addEventListener('input', evt => {
+//     console.log(evt);
+// });
+//---------------------------------------------------------------------------------
+// -- Подія focus, blur (часто викор в парі, blur, дуже схожий до change ЗНАЧЕННЯ ВИВОДИТЬСЯ ПРИ ВТРАТІ ФОКУСУ )
+
+// const input = document.querySelector('.input-label');
+// console.log(input);
+// input.addEventListener('blur', evt => {
+//     console.log(evt);
+// });
+//---------------------------------------------------------------------------------
+// -- Подія change (ПОДІЯ ВИВОДИТЬСЯ КОЛИ НАТИСНУЛИ ЕНТЕР)
+
+// const input = document.querySelector('.input-label');
+// console.log(input);
+// input.addEventListener('change', evt => {
+//     console.log(evt);
+// });
+//---------------------------------------------------------------------------------
