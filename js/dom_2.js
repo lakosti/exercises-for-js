2f2cbe07e3da6c3f00dab80e5153ad364ff188db
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-21 22:35:15 +0200

    add new markup

--- js/dom_2.js
+++ js/dom_2.js
@@ -270,3 +270,65 @@
 // });
 
 ////ЗАДАЧА добавити машини в розмітку і зробити пошук по моделі або марці_______________________________________
+
+const cars = [
+    {
+        id: 1,
+        car: 'Honda',
+        type: 'Civic',
+        price: 12000,
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU',
+    },
+    {
+        id: 2,
+        car: 'Audi',
+        type: 'Q7',
+        price: 40000,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg',
+    },
+    {
+        id: 33,
+        car: 'BMW',
+        type: '5 siries',
+        price: 9000,
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU',
+    },
+    {
+        id: 3,
+        car: 'Honda',
+        type: 'Accord',
+        price: 20000,
+        number: '+380000000000',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg',
+    },
+    {
+        id: 4,
+        car: 'Volvo',
+        type: 'XC60',
+        price: 7000,
+        img: 'https://www.volvocars.com/media/shared-assets/master/images/pages/my19/xc60-my19/accessories/xc60my19_accessories_exteriorfeature2_1.jpg?w=320',
+    },
+];
+
+const container = document.querySelector('.cars-lesson2');
+const searchBtn = document.querySelector('button');
+const form = document.querySelector('.search-car');
+
+const markup = cars
+    .map(({ id, car, type, price, img }) => {
+        return `<li data-id = "${id}">
+                  <img src="${img}" alt="${car}" width="300"/>
+                  <h2>Марка: ${car}</h2>
+                  <h3>Модель: ${type}</h3>
+                  <p>Ціна - ${price}</p>
+              </li>`;
+    })
+    .join('');
+container.insertAdjacentHTML('beforeend', markup);
+
+form.addEventListener('click', onClick);
+
+function onClick(evt) {
+    evt.preventDefault();
+    console.log(evt);
+}
