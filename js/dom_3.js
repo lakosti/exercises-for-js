e19158804944a6ded84ec2af39f4f5fa94fb6f43
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-25 20:15:38 +0200

    add new task

--- js/dom_3.js
+++ js/dom_3.js
@@ -1,7 +1,21 @@
-//ПРИ КЛІЦІ ОТРИМАТИ КОЛІР
+//ДЕЛЕГУВАННЯ ПОДІЙ --  коли додаємо прослуховувач на контейнер і слухаємо його дітей
+//ДЕЛЕГУВАННЯ ПОДІЙ КРАЩЕ ОБРОБЛЯТИ ЧЕРЕЗ КЛАСИ
 
-const boxes = document.querySelectorAll('.js-box');
-boxes.forEach(box => box.addEventListener('click', onClick));
+// evt.currentTarget --- ТОЙ ЯКИЙ ВИКЛИКАВ ПОДІЮ (батько)
+//evt.target --- ТОЙ НА ЯКОМУ ПОДІЯ СПРАЦЮВАЛА (дитина)};
+
+//ЗАДАЧА ПРИ КЛІЦІ ОТРИМАТИ КОЛІР (поганий приклад давати прослуховувача на всі кліки)
+
+const container = document.querySelector('.container-lesson_3');
+container.addEventListener('click', onClick);
+// [...container.children].forEach(item => item.addEventListener('click', onClick));
+
 function onClick(evt) {
-    console.dir(evt.target.dataset.color);
+    // console.dir(evt.currentTarget.dataset.color);
+
+    if (!evt.target.classList.contains('js-box')) {
+        return;
+    }
+    // console.log(evt.currentTarget); // ТОЙ ЯКИЙ ВИКЛИКАВ ПОДІЮ (батько)
+    console.log(evt.target.dataset.color); // ТОЙ НА ЯКОМУ ПОДІЯ СПРАЦЮВАЛА (дитина)};
 }
