9bd5054019552dcde23f06f3056e6dde79582249
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-25 20:24:09 +0200

    init pr

--- js/dom_3.js
+++ js/dom_3.js
@@ -6,16 +6,18 @@
 
 //ЗАДАЧА ПРИ КЛІЦІ ОТРИМАТИ КОЛІР (поганий приклад давати прослуховувача на всі кліки)
 
-const container = document.querySelector('.container-lesson_3');
-container.addEventListener('click', onClick);
-// [...container.children].forEach(item => item.addEventListener('click', onClick));
+// const container = document.querySelector('.container-lesson_3');
+// container.addEventListener('click', onClick);
+// // [...container.children].forEach(item => item.addEventListener('click', onClick));
 
-function onClick(evt) {
-    // console.dir(evt.currentTarget.dataset.color);
+// function onClick(evt) {
+//     // console.dir(evt.currentTarget.dataset.color);
 
-    if (!evt.target.classList.contains('js-box')) {
-        return;
-    }
-    // console.log(evt.currentTarget); // ТОЙ ЯКИЙ ВИКЛИКАВ ПОДІЮ (батько)
-    console.log(evt.target.dataset.color); // ТОЙ НА ЯКОМУ ПОДІЯ СПРАЦЮВАЛА (дитина)};
-}
+//     if (!evt.target.classList.contains('js-box')) {
+//         return;
+//     }
+//     // console.log(evt.currentTarget); // ТОЙ ЯКИЙ ВИКЛИКАВ ПОДІЮ (батько)
+//     console.log(evt.target.dataset.color); // ТОЙ НА ЯКОМУ ПОДІЯ СПРАЦЮВАЛА (дитина)};
+// }
+
+//ЗАДАЧА ДЕЛЕГУВАННЯ
