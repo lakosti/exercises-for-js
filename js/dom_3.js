6b5e388195836a835d542b5acd2e7f7e033217ec
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-26 20:35:27 +0200

    add markup

--- js/dom_3.js
+++ js/dom_3.js
@@ -20,4 +20,65 @@
 //     console.log(evt.target.dataset.color); // ТОЙ НА ЯКОМУ ПОДІЯ СПРАЦЮВАЛА (дитина)};
 // }
 
-//ЗАДАЧА ДЕЛЕГУВАННЯ
+///ПІДКЛЮЧЕННЯ БІБЛІОТЕКИ (СТВ МОДАЛКИ (метод show))
+
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`);
+console.log(instance);
+instance.show();
+
+//ЗАДАЧА ДЕЛЕГУВАННЯ (по айді клацуючи на зображення показувати його на весь екран) (CARS)__________________________
+
+const cars = [
+    {
+        id: 1,
+        car: 'Honda',
+        type: 'Civic',
+        price: 12000,
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU',
+    },
+    {
+        id: 2,
+        car: 'Audi',
+        type: 'Q7',
+        price: 40000,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg',
+    },
+    {
+        car: 'BMW',
+        type: '5 siries',
+        price: 9000,
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU',
+    },
+    {
+        id: 4,
+        car: 'Honda',
+        type: 'Accord',
+        price: 20000,
+        // number: '+380000000000',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg',
+    },
+    {
+        id: 5,
+        car: 'Volvo',
+        type: 'XC60',
+        price: 7000,
+        img: 'https://www.volvocars.com/media/shared-assets/master/images/pages/my19/xc60-my19/accessories/xc60my19_accessories_exteriorfeature2_1.jpg?w=320',
+    },
+];
+
+const container = document.querySelector('.js-container-3');
+console.log(container);
+
+const markup = cars
+    .map(
+        ({ id, car, img }) => `        <li data-id = '${id}'>
+            <img src="${img}" alt="${car}" width='300'/>
+            <p>Марка: ${car}</p>
+        </li>`
+    )
+    .join('');
+
+container.insertAdjacentHTML('beforeend', markup);
