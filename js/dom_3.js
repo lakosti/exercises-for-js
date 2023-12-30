41e560132219ff76d62d0a9c0f73ef5fe66d81bd
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-30 23:20:50 +0200

    add markup

--- js/dom_3.js
+++ js/dom_3.js
@@ -1,7 +1,10 @@
 // ДЕЛЕГУВАННЯ ПОДІЙ --  коли додаємо прослуховувач на контейнер і слухаємо його дітей
 // ДЕЛЕГУВАННЯ ПОДІЙ КРАЩЕ ОБРОБЛЯТИ ЧЕРЕЗ КЛАСИ
+//
 // closest() шукає першого спільного батька з таким класом /айді/селектором
 // closest() = document.querySelector()
+// evt.stopPropagination -- зупиняє подальше всплиття
+// evt.stopImmediatelyPropagination -- зупиняє не тільки всплиття а і обробку однакової події на поточному елементі;
 
 // evt.currentTarget --- ТОЙ ЯКИЙ ВИКЛИКАВ ПОДІЮ (батько)
 // evt.target --- ТОЙ НА ЯКОМУ ПОДІЯ СПРАЦЮВАЛА (дитина)};
@@ -125,3 +128,69 @@
 //     console.log(instance);
 //     instance.show();
 // }
+
+//////////////////////////////// ALEX ////////////////////////////////////
+
+//ЗАДАЧА РОБОТА З МОДУЛЬНОЮ БІБЛІОТЕКОЮ_______________________
+/**
+ * Створи картки з товарами на основі масиву products,
+ * приклад картки https://prnt.sc/KmgDlzqOIA3M
+ *
+ * Реалізуй делегування подій на колекції карток
+ * Після кліку на картку повинно з'являтись модальне вікно
+ * з детальною інформацією про продукт,
+ * приклад модального вікна https://prnt.sc/vWNoCeZcw7ii
+ *
+ * Для реалізації модального вікна використай
+ * бібліотеку basicLightbox (https://github.com/electerious/basicLightbox
+ */
+
+const products = [
+    {
+        id: 1,
+        img: 'https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG',
+        name: 'Monitor',
+        price: 3000,
+        description: '23-inch monitor with Full HD resolution.',
+    },
+    {
+        id: 2,
+        img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzWqRMI3HQiDfICHAmbArmaP4uOOIjfz0sDITv0dfkpb0mbbgX',
+        name: 'Laptop',
+        price: 20000,
+        description: 'Lightweight and powerful laptop with a 15-inch display and SSD.',
+    },
+    {
+        id: 3,
+        img: 'https://cdn.27.ua/799/66/39/6841913_1.jpeg',
+        name: 'Smartphone',
+        price: 8000,
+        description: 'Equipped with a triple camera and a multi-core processor.',
+    },
+    {
+        id: 4,
+        img: 'https://cdn.27.ua/799/b6/16/4371990_1.jpeg',
+        name: 'Tablet',
+        price: 12000,
+        description: '10-inch tablet with high performance and a Retina display.',
+    },
+];
+const phoneList = document.querySelector('.phone-list');
+const markup = products
+    .map(
+        ({ id, img, name, price }) => `            <li data-id='${id}'>
+                <div>
+        <img src="${img}" alt="${name}" width='300'>
+                    <h3>${name}</h3>
+                    <p>Ціна: ${price} грн</p>
+                </div>
+            </li>`
+    )
+    .join('');
+
+phoneList.insertAdjacentHTML('beforeend', markup);
+phoneList.addEventListener('click', onClick);
+
+function onClick(evt) {
+    //
+}
