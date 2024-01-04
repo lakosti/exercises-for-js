2306fb516ffac925700f7bfabd3adc62c603dce8
Yaroslava <yaroslava.hovenko@gmail.com>
2024-01-04 21:53:34 +0200

    add new task

--- js/mentorDom.js
+++ js/mentorDom.js
@@ -1,1 +1,20 @@
-//  TASK 1
+//  TASK 1 BASE WORK WITH DOM
+const bodyEl = document.body; // лише боді так
+// console.log(bodyEl);
+
+const h1EL = document.querySelector('#title');
+
+const dataEls = document.querySelectorAll('[data-topic]');
+// console.log(dataEls);
+
+const lastEl = document.querySelector('[data-topic="manipulation"]');
+// console.log(lastEl);
+// console.log(dataEls[dataEls.length - 1]);
+
+//сусід
+
+const firstSibling = h1EL.nextElementSibling;
+// console.log(firstSibling);
+
+const titles = document.querySelectorAll('h3');
+titles.forEach(title => console.log(title.textContent));
