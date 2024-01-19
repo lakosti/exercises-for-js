4ea8758d2a61cc1e76e42b07dc4400eb4bf13777
Yaroslava <yaroslava.hovenko@gmail.com>
2024-01-19 23:30:40 +0200

    example advertising

--- js/async.js
+++ js/async.js
@@ -37,3 +37,26 @@
 // console.log(currentDay);
 
 // -----------------------------приклад РЕКЛАМИ------------------------
+
+// const box = document.querySelector('.div-cont');
+// const timer = document.querySelector('.js-timer');
+// let counter = 11;
+
+// setTimeout(() => {
+//     box.style.display = 'block';
+//     const id = setInterval(() => {
+//         counter -= 1;
+//         timer.textContent = counter;
+
+//         //якщо секунди дійшли до 0 то видаляємо інтервал
+//         if (!counter) {
+//             clearInterval(id);
+//             timer.textContent = 'X';
+//             timer.addEventListener('click', onClick);
+//         }
+//     }, 1000); // викликаємо інтервал (повторення) через секунду
+// }, 5000); // затримка 5 сек (реклама з'явиться через 5 секунд (лише раз))
+
+// function onClick() {
+//     box.style.display = 'none';
+// }
