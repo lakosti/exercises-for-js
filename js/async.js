825e9dfea0aeb4c9752a749a7d285783c0c1f93a
Yaroslava <yaroslava.hovenko@gmail.com>
2024-01-20 18:50:09 +0200

    add a few simple task

--- js/async.js
+++ js/async.js
@@ -62,3 +62,60 @@
 // }
 
 // ----------------------------------------- ГОДИННИК АРТЕМА ----------------------------------------------
+
+//ЩОБ ПРАЦЮВАТИ З ОБ'ЄКТОМ ДЕЙТ ОБОВ'ЯЗКОВО ОГОЛОШУЄМО ЙОГО
+
+const date = new Date();
+
+//1. ДЕНЬ ТИЖНЯ getDay()
+const dayOfWeek = document.querySelector('.date-day');
+const daysOfWeeks = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П`ятниця', 'Субота'];
+const currentDay = daysOfWeeks[date.getDay()]; //субота
+// const currentDay = date.getDay(); // 6
+dayOfWeek.textContent = currentDay;
+
+//2. ДЕНЬ МІСЯЦЬ getDate()
+const dayOfMonth = document.querySelector('.date');
+const currentDayOfMonth = date.getDate();
+dayOfMonth.textContent = currentDayOfMonth;
+
+//3. МІСЯЦЬ getMonth()
+const month = document.querySelector('.date-month');
+const namesOfMonth = [
+    'Січень',
+    'Лютий',
+    'Березень',
+    'Квітень',
+    'Травень',
+    'Червень',
+    'Липень',
+    'Серпень',
+    'Вересень',
+    'Жовтень',
+    'Листопад',
+    'Грудень',
+];
+const currentMonth = namesOfMonth[date.getMonth()];
+month.textContent = currentMonth;
+
+//4. РІК getFullYear()
+const year = document.querySelector('.date-year');
+const currentYear = date.getFullYear();
+year.textContent = currentYear;
+
+//  ------------------ таймер від 10 до 1 --------------------------
+
+// const timeCounter = document.querySelector('.digital-clock');
+// let counter = 11;
+// const id = setInterval(() => {
+//     counter -= 1;
+//     timeCounter.textContent = counter;
+//     if (!counter) {
+//         clearInterval(id);
+//         timeCounter.textContent = 'The End';
+//     }
+// }, 1000);
+///
+
+///ГОДИННИК
+const timeCounter = document.querySelector('.digital-clock');
