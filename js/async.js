67ee71e4e5e025b9fc7beabf9b92710d14b211e5
Yaroslava <yaroslava.hovenko@gmail.com>
2024-01-20 22:44:27 +0200

    add new task

--- js/async.js
+++ js/async.js
@@ -65,44 +65,64 @@
 // ----------------------------------------- ГОДИННИК АРТЕМА ----------------------------------------------
 
 //ЩОБ ПРАЦЮВАТИ З ОБ'ЄКТОМ ДЕЙТ ОБОВ'ЯЗКОВО ОГОЛОШУЄМО ЙОГО
+//ПРИ РОБОТІ З ЧАСОМ НЕОБХІДНО ВИКОРИСТОВУВАТИ СЕТ ІНТЕРВАЛ (шоб оновлювались дані на сайті кожної секунди)
 
-const date = new Date();
-
-//1. ДЕНЬ ТИЖНЯ getDay()
-const dayOfWeek = document.querySelector('.date-day');
-const daysOfWeeks = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П`ятниця', 'Субота'];
-const currentDay = daysOfWeeks[date.getDay()]; //субота
-// const currentDay = date.getDay(); // 6
-dayOfWeek.textContent = currentDay;
-
+//1. ДЕНЬ ТИЖНЯ getDay() + масив
 //2. ДЕНЬ МІСЯЦЬ getDate()
-const dayOfMonth = document.querySelector('.date');
-const currentDayOfMonth = date.getDate();
-dayOfMonth.textContent = currentDayOfMonth;
-
-//3. МІСЯЦЬ getMonth()
-const month = document.querySelector('.date-month');
-const namesOfMonth = [
-    'Січень',
-    'Лютий',
-    'Березень',
-    'Квітень',
-    'Травень',
-    'Червень',
-    'Липень',
-    'Серпень',
-    'Вересень',
-    'Жовтень',
-    'Листопад',
-    'Грудень',
-];
-const currentMonth = namesOfMonth[date.getMonth()];
-month.textContent = currentMonth;
-
+//3. МІСЯЦЬ getMonth() + масив
 //4. РІК getFullYear()
-const year = document.querySelector('.date-year');
-const currentYear = date.getFullYear();
-year.textContent = currentYear;
+
+//отримуємо через дом доступ до даних
+// const dayOfWeek = document.querySelector('.date-day');
+// const dayOfMonth = document.querySelector('.date');
+// const month = document.querySelector('.date-month');
+// const year = document.querySelector('.date-year');
+// const timeCounter = document.querySelector('.digital-clock');
+
+// const daysOfWeeks = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П`ятниця', 'Субота'];
+
+// const namesOfMonth = [
+//     'Січень',
+//     'Лютий',
+//     'Березень',
+//     'Квітень',
+//     'Травень',
+//     'Червень',
+//     'Липень',
+//     'Серпень',
+//     'Вересень',
+//     'Жовтень',
+//     'Листопад',
+//     'Грудень',
+// ];
+
+// //ПРИ РОБОТІ З ЧАСОМ НЕОБХІДНО ВИКОРИСТОВУВАТИ СЕТ ІНТЕРВАЛ (шоб оновлювались дані на сайті кожної секунди)
+
+// setInterval(() => {
+//     //обов'язково поміщаємо сюди об'єкт ДЕЙТ
+//     const date = new Date();
+
+//     //отримуємо актуальні дані з дейту
+//     const currentDay = daysOfWeeks[date.getDay()]; //субота
+//     const currentDayOfMonth = date.getDate();
+//     const currentMonth = namesOfMonth[date.getMonth()];
+//     const currentYear = date.getFullYear();
+
+//     //електро годинник
+//     const currentHours = date.getHours();
+//     const currentMinutes = date.getMinutes();
+//     const currentSeconds = date.getSeconds();
+//     const currentTimeFormat = `${currentHours.toString().padStart(2, '0')} : ${currentMinutes
+//         .toString()
+//         .padStart(2, '0')} : ${currentSeconds.toString().padStart(2, '0')}`;
+
+//     //присвоюємо дані з дейту до ДОМ
+//     dayOfWeek.textContent = currentDay;
+//     dayOfMonth.textContent = currentDayOfMonth;
+//     month.textContent = currentMonth;
+//     year.textContent = currentYear;
+//     timeCounter.textContent = `Поточний час: ${currentTimeFormat}`;
+// }, 1000);
 
 //  ------------------ таймер від 10 до 1 --------------------------
 
@@ -117,8 +137,37 @@ year.textContent = currentYear;
 //         timeCounter.textContent = 'The End';
 //     }
 // }, 1000);
-///
 
-///ГОДИННИК
+/// ----------------------------- ЗВОРОТНІЙ ВІДЛІК ----------------------------
 
-const timeCounter = document.querySelector('.digital-clock');
+// const text = document.querySelector('.time');
+// setInterval(() => {
+//     const date = new Date();
+//     const targetDate = new Date('01/23/2024'); //рандомна дата
+//     const substr = convertMs(targetDate - date);
+//     const { days = 0, hours = 0, minutes = 0, seconds } = substr;
+//     if (!days) {
+//         text.textContent = `${hours} hour ${minutes} minutes ${seconds} seconds`;
+//     } else {
+//         text.textContent = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+//     }
+// }, 1000);
+
+// function convertMs(ms) {
+//     // Number of milliseconds per unit of time
+//     const second = 1000;
+//     const minute = second * 60;
+//     const hour = minute * 60;
+//     const day = hour * 24;
+
+//     // Remaining days
+//     const days = Math.floor(ms / day);
+//     // Remaining hours
+//     const hours = Math.floor((ms % day) / hour);
+//     // Remaining minutes
+//     const minutes = Math.floor(((ms % day) % hour) / minute);
+//     // Remaining seconds
+//     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//     return { days, hours, minutes, seconds };
+// }
