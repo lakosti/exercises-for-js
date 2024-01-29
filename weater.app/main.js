3da750463ed73e8e68b9e76c5c45e033cb1b945b
Yaroslava <yaroslava.hovenko@gmail.com>
2024-01-29 22:11:46 +0200

    example promise

--- weater.app/main.js
+++ weater.app/main.js
@@ -20,7 +20,7 @@
 // function getWeather(city, days) {
 //     // http://api.weatherapi.com/v1/forecast.json?key=f652ad30535645538aa180322242701&q=Paris&days=5
 
-//     // ФОРМУЄМО ЮРЛ ЗАПИТ (запит на бекенд)
+//     // ФОРМУЄМО ЮРЛ ЗАПИТ (запит на бекенд або HTTP запит)
 //     const BASE_URL = 'http://api.weatherapi.com/v1';
 //     const API_KEY = 'f652ad30535645538aa180322242701'; // при реєстрації отримуємо
 
