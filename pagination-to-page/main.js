da38700de4e370a687ede82ee435d8d687549330
Yaroslava <yaroslava.hovenko@gmail.com>
2024-02-16 00:42:06 +0200

    add pagination task

--- pagination-to-page/main.js
+++ pagination-to-page/main.js
@@ -3,3 +3,19 @@
  * Використовуй поширення подій.Додавай клас`active` на поточну сторінку. Створи допоміжну функцію`updateActivePage`, яка повинна обновляти активний клас.
  * Функція`handleClick`повинна викликатися коли відбувається клік на елементі`pagination`.
  */
+
+const list = document.querySelector('.js-pagination');
+list.addEventListener('click', onClick);
+
+function onClick({ target }) {
+    if (target.dataset.type !== 'page') {
+        return;
+    }
+    //видаляємо колір з усіх
+    document.querySelectorAll('.js-pagination .active').forEach(element => {
+        element.classList.remove('active');
+    });
+
+    // і потім при кліці додаємо лише на один вибраний
+    target.classList.add('active');
+}
